67b044de64872ae023f09891e986098fb6aaa3ce
ddthetasinetheta <34762521+ddthetasinetheta@users.noreply.github.com>
2018-01-24 17:16:59 -0800

    Add files via upload

---
+++ dist/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+app.listen(9000, function (){
+    console.log('Server running...');
+});
